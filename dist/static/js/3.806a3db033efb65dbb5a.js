efa46f01bf8a29ad7a3247e0988116f29e8cd533
PURPLEPENG0_0 <sunpengpeng@geek-zoo.com>
2018-03-21 11:46:22 +0800

    添加dist目录

---
+++ dist/static/js/3.806a3db033efb65dbb5a.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],{X6d5:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var a={data:function(){return{carData:""}},components:{tabBar:n("GlpD").a},mounted:function(){}},r={render:function(){var t=this.$createElement,e=this._self._c||t;return e("div",{attrs:{id:"demo"}},[e("tab-bar")],1)},staticRenderFns:[]};var o=n("VU/8")(a,r,!1,function(t){n("boQj")},"data-v-140a6b16",null);e.default=o.exports},boQj:function(t,e){}});
+//# sourceMappingURL=3.806a3db033efb65dbb5a.js.map
