38e638b14345103050d9ea2da28a3310887eafee
PURPLEPENG0_0 <sunpengpeng@geek-zoo.com>
2018-04-18 16:13:47 +0800

    fix: 更新个人资料后，saveUser

--- src/page/profile/ProfileInfo.vue
+++ src/page/profile/ProfileInfo.vue
@@ -67,7 +67,7 @@ export default {
 	methods: {
 		...mapMutations({
 			clearToken: 'signout',
-			signin: 'signin'
+			saveUser: 'saveUser'
 		}),
 
 		/*
@@ -101,7 +101,7 @@ export default {
 			let params = this.params;
 			userProfileUpdate(params.values, params.gender, params.nickname, params.avatar_url).then(res => {
 				if (res) {
-					this.signin({'user': res.user});
+					this.saveUser({'user': res.user});
 				}
 			})
 		}
