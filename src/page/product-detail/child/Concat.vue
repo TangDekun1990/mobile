fbc112b7dc162e6891cb4c3222bf85cf61da3020
GKS air 03 <zhanglimin@geek-zoo.com>
2018-04-27 15:59:01 +0800

    对接智齿客服 优化

--- src/page/product-detail/child/Concat.vue
+++ src/page/product-detail/child/Concat.vue
@@ -18,30 +18,42 @@
 		},
 
 		computed: mapState({
-			user: state => state.auth.user
+			user: state => state.auth.user,
+			detailInfo: state => state.detail.detailInfo
 		}),
 
+		// TODO 解决detailInfo 一开始没有的情况
+		watch: {
+			detailInfo: function(value) {
+				if (value) {
+					this.initzhichi();
+				}
+			}
+		},
+
 		created(){
-			var zhiManager = (getzhiSDKInstance());
-		    //手动初始化时 通过 load 方法调用 js 组件
-		    zhiManager.on("load", function(ret) {
-		        zhiManager.initBtnDOM();
-		        zhiManager.set('lazyFlag', true);
-		        zhiManager.set('robotHelloWord', '您好! 我是机器人小超， 有什么吩咐您请说哦！');
-		        zhiManager.set('thumbnail_info', 'http://www.sobot.com/images/logo.png');
-		        zhiManager.set('label_info', '200');
-		        zhiManager.set('abstract_info', '更精确的理解，更精确地回答，在闲聊寒暄中，拉近与客户的距离，导入知识库，意想不到的方便快捷，最简单的知识添加，事半功倍');
-		        zhiManager.set('url_info', 'http://www.sobot.com/images/logo.png');
-		        zhiManager.set('title_info', '智齿_商品详情页_小智智能');
-		        zhiManager.set('back', 1);
-		        zhiManager.set('titleFlag', 2);
-		        zhiManager.set('customTtile', '咨询客服')
-		        zhiManager.set('customBtn', 'true');
-		        zhiManager.set('title', '咨询在线客服');
-		    });
 		},
 
 		methods: {
+			/*
+			 * initzhichi: 初始化智齿
+			 */
+			initzhichi() {
+				var data = this.detailInfo;
+				var zhiManager = (getzhiSDKInstance());
+			    //手动初始化时 通过 load 方法调用 js 组件
+			    zhiManager.on("load", function(ret) {
+			        zhiManager.initBtnDOM();
+			        zhiManager.set('lazyFlag', true);
+			        zhiManager.set('robotHelloWord', '您好! 我是机器人小超， 有什么吩咐您请说哦！');
+			        // 设置商品浮层的数据
+			        zhiManager.set('thumbnail_info', data.photos[0].thumb);
+			        zhiManager.set('label_info', data.current_price);
+			        zhiManager.set('abstract_info', data.desc);
+			        zhiManager.set('url_info', 'http://192.168.10.18:8080/#/detial/'+data.id+'');
+			        zhiManager.set('title_info', data.name);
+			    });
+			}
 		}
 	}
 </script>
