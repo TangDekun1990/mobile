35771dee4eb7a919936011b84741530121cf4724
PURPLEPENG0_0 <sunpengpeng@geek-zoo.com>
2018-05-04 16:20:45 +0800

    update dist

--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,8 @@ let apiBaseUrl = '';
 let photoBaseUrl = '';
 
 if (process.env.NODE_ENV === 'development') {
-	apiBaseUrl = 'http://api.wenchao.dev.geek-zoo.cn';
-	// apiBaseUrl = 'http://api.wenchao.pre.geek-zoo.cn';
+	// apiBaseUrl = 'http://api.wenchao.dev.geek-zoo.cn';
+	apiBaseUrl = 'http://api.wenchao.pre.geek-zoo.cn';
 	// apiBaseUrl = 'http://api.wenchao.ae';
 } else if (process.env.NODE_ENV === 'production') {
 	apiBaseUrl = 'http://api.wenchao.pre.geek-zoo.cn';
