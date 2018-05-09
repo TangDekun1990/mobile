18368d84858e6e297bfe46ceb648068ec3907ed0
GKS air 03 <zhanglimin@geek-zoo.com>
2018-05-09 16:42:05 +0800

    修改api地址为正式环境

--- src/config/env.js
+++ src/config/env.js
@@ -6,9 +6,9 @@ if (process.env.NODE_ENV === 'development') {
 	apiBaseUrl = 'http://api.wenchao.pre.geek-zoo.cn';
 	// apiBaseUrl = 'http://api.wenchao.ae';
 } else if (process.env.NODE_ENV === 'production') {
-	apiBaseUrl = 'http://api.wenchao.pre.geek-zoo.cn';
+	// apiBaseUrl = 'http://api.wenchao.pre.geek-zoo.cn';
 	// apiBaseUrl = 'http://api.wenchao.dev.geek-zoo.cn';
-	// apiBaseUrl = 'http://api.wenchao.ae';
+	apiBaseUrl = 'http://api.wenchao.ae';
 }
 
 // 版本号
