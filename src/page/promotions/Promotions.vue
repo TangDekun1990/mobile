89b9d31f6a3aff2fd8e8f505129b41e7f723f64c
GKS air 03 <zhanglimin@geek-zoo.com>
2018-05-11 16:27:48 +0800

    促销列表

---
+++ src/page/promotions/Promotions.vue
@@ -0,0 +1,157 @@
+<!-- Promotions.vue -->
+<template>
+	<div class="promotions-list-wrapper recommend-wrapper">
+		<div class="ui-commmon-header">
+			<img src="../../assets/image/change-icon/back@2x.png" v-on:click='goBack()' class="ui-go-back">
+			<h3>{{ promotionsList.title }}</h3>
+			<img src="../../assets/image/change-icon/b2_cart@2x.png" class="ui-cart" v-on:click='goCart()'>
+			<span class="cart-number" v-if="cartNumber <= 100 && cartNumber > 0">{{ cartNumber }}</span>
+			<span class="cart-number" v-if="cartNumber >= 100  && cartNumber > 0 ">99+</span>
+		</div>
+		<div class="ui-recommend-body" v-infinite-scroll="getMore" infinite-scroll-disabled="loading" infinite-scroll-distance="10">
+			<!-- <v-recommend-list v-for="(item, index) in recommendList" :item="item" :productId="item.id" v-bind:key="index"></v-recommend-list> -->
+			<v-promotions-list v-for="(item, index) in products" :item="item" :productId="item.id" v-bind:key="index"></v-promotions-list>
+			<p class="recommend-no-more" v-if='!isMore'>没有更多了</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	import { cartQuantity } from '../../api/network/cart'
+	import { productActivity } from '../../api/network/product'
+	import { mapState, mapMutations } from 'vuex'
+	import productList from '../product/child/ProduceBody';
+	export default {
+		data() {
+			return {
+				loading: false,
+				isMore: true,
+				params: {'page': 0, 'per_page': 10},
+				promotionsList: {},
+				products: []
+			}
+		},
+
+		components: {
+			'v-promotions-list': productList
+		},
+
+		created(){
+			this.$on('get-cart-quantity', () => {
+				this.getCarNumber();
+			})
+		},
+
+		computed: mapState({
+			user: state => state.auth.user,
+			cartNumber: state => state.tabBar.cartNumber
+		}),
+
+		mounted(){
+			// 计算内容高度
+		  //   this.$nextTick( () => {
+		  //   	this.target = document.querySelector('.ui-recommend-body');
+		  //   	let totalHeight = 45;
+				// const target = this.target;
+		  //   	this.utils.fillTheScreen({target, totalHeight});
+		  //   });
+		},
+
+		methods: {
+			...mapMutations({
+				setCartNumber: 'setCartNumber'
+			}),
+
+			/*
+			 *  getCarNumber: 获取购物车数量
+			 */
+			getCarNumber() {
+				cartQuantity().then(res => {
+					if (res) {
+						this.setCartNumber(res.quantity);
+					}
+				})
+			},
+
+			/*
+			 * goBack: 返回到上一页
+			 */
+			goBack() {
+				this.$router.go(-1);
+			},
+
+			/*
+			 * getMore: 无限滚动
+			 */
+			getMore() {
+				this.loading = true;
+				this.params.page = ++this.params.page;
+				if (this.isMore) {
+					this.loading = false;
+					this.getPromotionsList();
+				}
+			},
+
+			/*
+			 *  goCart: 跳转到购物车
+			 */
+			goCart() {
+				if (this.user) {
+					this.$router.push({'name': 'cart', 'params': {type: 0}});
+				} else {
+					this.$router.push({'name': 'signin'});
+				}
+			},
+
+			/*
+			 * getPromotionsList: 获取促销列表
+			 */
+			getPromotionsList(){
+				let params = Object.assign({}, this.params, this.$route.query);
+				console.log(params);
+				productActivity(params.id, params.page, params.per_page).then( res => {
+					if (res) {
+						this.promotionsList = Object.assing({}, res.products);
+						this.products = this.products.concat(res.products);
+						this.isMore = res.paged.more;
+					}
+				})
+			}
+
+		}
+	}
+</script>
+
+<style lang='scss' scoped>
+.recommend-wrapper {
+	background-color: #ffffff;
+	.ui-commmon-header {
+		border-bottom: 1px solid #E8EAED;
+		background-color: #ffffff;
+		position: absolute;
+		top: 0px;
+		width: 100%;
+		padding: 0px;
+		span.cart-number {
+			right: 15px;
+		}
+	}
+	.ui-recommend-body {
+		position: absolute;
+		top: 45px;
+		background-color: #ffffff;
+		width: 100%;
+		height: auto;
+		overflow: auto;
+	}
+	.recommend-no-more {
+		color: #7C7F88;
+		font-size: 12px;
+		padding: 0px;
+		margin: 0px;
+		text-align: center;
+    padding: 10px 0px;
+	}
+}
+</style>
+
