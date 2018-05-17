92e208eee2ea96040c33ac80e3f2122661ab6854
GKS air 03 <zhanglimin@geek-zoo.com>
2018-05-17 14:27:28 +0800

    fix  bug

--- src/page/product-detail/index.vue
+++ src/page/product-detail/index.vue
@@ -56,6 +56,14 @@
 			})
 		},
 
+		beforeRouteLeave (to, from , next) {
+			console.log('beforeRouteLeave');
+			if (from.name == 'detail') {
+				window.location.reload();
+			}
+			next();
+		},
+
 		// watch: {
 		//     $route (to, from) {
 		//     	window.location.reload();
